8586b9a2e3156631d4c47bc08f85d1eaf40c941c
wasd003 <wasd003@sjtu.edu.cn>
2024-01-31 20:37:28 +0800

    inline static variable

--- answer/oop/static-class-member.cxx
+++ answer/oop/static-class-member.cxx
@@ -1,12 +1,23 @@
 #include <quiz/base.h>
 #include <quiz/log.h>
 
+struct test {
+    int a, b, c;
+    constexpr test(int a, int b, int c) : a(a), b(b), c(c) {}
+};
+
 struct widget {
     // const propgation
     static const int N = 100;
     static const int M;
     static const std::string const_string;
     static std::string non_const_string;
+
+    // inline static variable
+    inline static std::string str = "I am inline static variable";
+
+    // constexpr data is implict inline since c++17
+    constexpr static test test_obj {1, 2, 3};
 };
 
 const std::string widget::const_string = "const-string";
@@ -16,5 +27,8 @@ const int widget::M = 200;
 int main() {
     auto x = widget::N;
     debug(x, " ", widget::M, " ", widget::const_string, " ", widget::non_const_string);
+    auto& sr = widget::str;
+    std::cout << sr << std::endl;
+    debug(widget::test_obj.a);
     return 0;
 }
--- doc/oop/static-class-member.md
+++ doc/oop/static-class-member.md
@@ -1,4 +1,6 @@
 ## 题目描述
 本题主要考察static类成员变量的初始化。
-1. static非const int成员变量如何初始化？
-2. static const int成员变量还可以怎样初始化？
+1. static非const 整数成员变量如何初始化？
+2. static const 整数成员变量还可以怎样初始化？
+3. 对于header only的库，如果要定义static数据成员怎么办？
+4. constexpr static数据成员如何初始化？
