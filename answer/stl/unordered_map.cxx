c10eef4c1f7a75d9cdd52f999bec8313fde4847c
wasd003 <wasd003@sjtu.edu.cn>
2024-02-24 16:53:46 +0800

    unordered_map

---
+++ answer/stl/unordered_map.cxx
@@ -0,0 +1,26 @@
+#include <functional>
+#include <quiz/base.h>
+#include <unordered_map>
+
+struct test {
+    int a, b, c;
+    bool operator==(const test& rhs) const {
+        return a == rhs.a && b == rhs.b && c == rhs.c;
+    }
+};
+
+namespace std {
+    template<>
+    struct hash<test> {
+        size_t operator()(const test& t) const {
+            return std::hash<int>{}(t.a) ^ std::hash<int>{}(t.b) ^ std::hash<int>{}(t.c);
+        }
+    };
+}
+
+int main() {
+    std::unordered_map<test, int> h;
+    h[{1, 2, 3}] = 4;
+    std::cout << h[{1, 2, 3}];
+    return 0;
+}
