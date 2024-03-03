c00e8d0c79db9b2bf07a0b90cfceb272c5ea2485
wasd003 <wasd003@sjtu.edu.cn>
2024-03-03 18:39:20 +0800

    update shared_ptr

--- answer/mm/shared_ptr.cxx
+++ answer/mm/shared_ptr.cxx
@@ -2,41 +2,50 @@
 #include <memory>
 #include <quiz/base.h>
 #include <atomic>
-
-template<typename Del>
-struct shared_ptr_control_block {
-    std::atomic<int> count;
-    Del del;
-
-    shared_ptr_control_block(int cnt, Del del) : count(cnt), del(del) {}
-};
+#include <functional>
 
 template<typename T>
-struct default_del_op {
-    void operator()(T* ptr) {
-        std::cout << "del";
+struct default_deleter {
+    void operator()(T *ptr) {
+        std::cout << "default_deleter";
         delete ptr;
     }
 };
 
 template<typename T>
-struct test_del_op {
-    void operator()(T* ptr) {
-        std::cout << "test del";
-        delete ptr;
-    }
+struct control_block {
+    std::atomic<int> refcnt;
+    std::function<void(T*)> deleter;
+
+    control_block(int refcnt, std::function<void(T*)> deleter):
+        refcnt(refcnt), deleter(deleter) {}
 };
 
-template<typename T, typename Del = default_del_op<T>>
+template<typename T>
 struct shared_ptr {
-    T *ptr;
-    shared_ptr_control_block<Del> *cb;
+    T *data;
+    control_block<T> *cb;
 
-    shared_ptr(T* ptr, const Del& del = Del {}) : ptr(ptr), cb(new shared_ptr_control_block {1, del}) {}
+    shared_ptr(T *data, std::function<void(T*)> deleter = default_deleter<T> {}):
+        data(data), cb(new control_block<T> {1, deleter}) {}
 
-    shared_ptr(const shared_ptr& rhs) : ptr(rhs.ptr), cb(rhs.cb) {
-        assert(cb);
-        cb->count ++ ;
+    shared_ptr() : data(nullptr), cb(nullptr) {}
+
+    T *get() {
+        return data;
+    }
+
+    T *operator->() {
+        return data;
+    }
+
+    void swap(shared_ptr &rhs) {
+        std::swap(data, rhs.data);
+        std::swap(cb, rhs.cb);
+    }
+
+    shared_ptr(const shared_ptr& rhs) : data(rhs.data), cb(rhs.cb) {
+        cb->refcnt ++ ;
     }
 
     shared_ptr& operator=(const shared_ptr& rhs) {
@@ -45,9 +54,8 @@ struct shared_ptr {
         return *this;
     }
 
-    shared_ptr(shared_ptr&& rhs) : ptr(rhs.ptr), cb(rhs.cb) {
-        assert(cb);
-        rhs.ptr = nullptr;
+    shared_ptr(shared_ptr&& rhs) : data(rhs.data), cb(rhs.cb) {
+        rhs.data = nullptr;
         rhs.cb = nullptr;
     }
 
@@ -57,15 +65,18 @@ struct shared_ptr {
         return *this;
     }
 
-    void swap(shared_ptr& rhs) noexcept {
-        swap(ptr, rhs.ptr);
-        swap(cb, rhs.cb);
+    ~shared_ptr() {
+        auto oldcnt = cb->refcnt.fetch_sub(1);
+        if (oldcnt == 1) {
+            cb->deleter(data);
+            data = nullptr;
+            delete cb;
+            cb = nullptr;
+        }
     }
 
-    ~shared_ptr() {
-        if ( -- cb->count == 0) {
-            cb->del(ptr);
-        }
+    operator bool() {
+        return data != nullptr;
     }
 };
 
@@ -73,8 +84,31 @@ struct test {
     int a, b, c;
 };
 
+template<typename T>
+struct test_del_op {
+    void operator()(T* ptr) {
+        std::cout << "test_del_op";
+        delete ptr;
+    }
+};
+
+
+template<typename T, typename... Args>
+shared_ptr<T> make_shared(Args&&... args) {
+    auto ptr = new uint8_t [sizeof (T) + sizeof (control_block<T>)];
+    auto sp = shared_ptr<T> {};
+    sp.data = reinterpret_cast<T*>(ptr);
+    sp.cb = reinterpret_cast<control_block<T>*>(ptr + sizeof (T));
+    new(ptr) T(std::forward<Args>(args)...);
+    return sp;
+}
+
 int main() {
     auto ptr = new test {1, 2, 3};
-    auto sp = shared_ptr<test, test_del_op<test>>(ptr);
+    auto sp = shared_ptr<test>(ptr, test_del_op<test>{});
+
+    using PII = std::pair<int, int>;
+    auto ssp = make_shared<PII>(1, 2);
+    printf("%d-%d\n", ssp->first, ssp->second);
     return 0;
 }
--- doc/mm/shared_ptr.md
+++ doc/mm/shared_ptr.md
@@ -2,3 +2,4 @@
 1. 实现shared_ptr，要求支持自定义删除器。
 2. 如何判断shared_ptr是否为空指针？
 3. 在自己实现的shared_ptr中支持判空
+4. 实现make_shared
