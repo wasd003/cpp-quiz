55df21ae095f4ba367f2959223262a815ed8c240
wasd003 <wasd003@sjtu.edu.cn>
2024-05-07 11:40:18 +0800

    type erasue

---
+++ answer/template/type-erase.cxx
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+#include <type_traits>
+#include <mutex>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <tuple>
+#include <cassert>
+#include <type_traits>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <queue>
+#include <mutex>
+
+struct any {
+private:
+    struct execution_interface {
+        virtual void execute() = 0;
+
+        virtual ~execution_interface() {}
+    };
+
+    template<typename Tsk, typename... Args>
+    struct execution_engine : public execution_interface {
+    private:
+        using R = std::invoke_result_t<Tsk, Args...>;
+        std::packaged_task<R(Args...)> tsk;
+        std::tuple<Args...> args;
+
+    public:
+        virtual void execute() override {
+            std::apply(tsk, args);
+        }
+
+        execution_engine(Tsk _tsk, Args&&... _args) :
+            tsk(std::move(_tsk)),
+            args(std::forward<Args>(_args)...)
+        {}
+
+        virtual ~execution_engine() {}
+    };
+
+    execution_interface *pimpl {nullptr};
+
+    void swap(any& rhs) noexcept {
+        std::swap(pimpl, rhs.pimpl);
+    }
+
+public:
+    void execute() {
+        assert(pimpl);
+        pimpl->execute();
+    }
+
+    template<typename Tsk, typename... Args>
+    any(Tsk tsk, Args&&... args) {
+        pimpl = new execution_engine<Tsk, Args...> {
+            std::move(tsk),
+            std::forward<Args>(args)...
+        };
+    }
+
+    any(const any& rhs) = delete;
+
+    any& operator=(const any& rhs) = delete;
+
+    any(any&& rhs) : pimpl(rhs.pimpl) {
+        rhs.pimpl = nullptr;
+    }
+
+    any& operator=(any&& rhs) {
+        auto tmp = std::move(rhs);
+        swap(tmp);
+        return *this;
+    }
+
+    ~any() {
+        delete pimpl;
+    }
+};
+
+static std::deque<any> vec;
+static constinit std::mutex mtx;
+static std::condition_variable cv;
+
+template<typename F, typename... Args>
+decltype(auto) submit(F f, Args&&... args) {
+    using R = std::invoke_result_t<F, Args...>;
+    std::packaged_task<R(Args...)> tsk {f};
+    auto fut = tsk.get_future();
+    {
+        std::unique_lock<std::mutex> lk {mtx};
+        vec.emplace_back(std::move(tsk), std::forward<Args>(args)...);
+    }
+    cv.notify_all();
+    return fut;
+}
+
+void test_case1() {
+    auto f = [](int a, int b) {
+        std::cout << a << "+" << b << "=" << a + b << std::endl;
+        return a + b;
+    };
+
+    std::thread worker([]() {
+        for (;;) {
+            std::unique_lock<std::mutex> lk {mtx};
+            cv.wait(lk, []() { return !vec.empty(); });
+            while (vec.size()) {
+                auto cur = std::move(vec.front());
+                vec.pop_front();
+                cur.execute();
+            }
+        }
+    });
+
+    auto fut = submit(f, 3, 4);
+    auto ret = fut.get();
+    std::cout << "real return:" << ret << std::endl;
+
+    worker.join();
+}
+
+int main() {
+    test_case1();
+
+    return 0;
+}
