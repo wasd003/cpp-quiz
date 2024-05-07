24f37de6b731fe2421b78118e95bc2f3267f7526
wasd003 <wasd003@sjtu.edu.cn>
2024-05-07 17:18:05 +0800

    feat/variadic-template: printk

--- answer/template/variadic-template.cxx
+++ answer/template/variadic-template.cxx
@@ -16,8 +16,37 @@ int fold_add(First first, Rest... rest) {
     return (first + ... + rest);
 }
 
-int main() {
+void printk(const std::string_view &s) {
+    std::cout << s;
+}
+
+template<typename First, typename... Rest>
+void printk(const std::string_view &s, const First& first, const Rest&... rest) {
+    if (s.empty()) return;
+    if (s.size() >= 2 && s[0] == '{' && s[1] == '}') [[unlikely]] {
+        std::cout << first;
+        printk(s.substr(2), rest...);
+    } else {
+        std::cout << s[0];
+        printk(s.substr(1), first, rest...);
+    }
+}
+
+void test_add() {
     auto ans1 = recursive_add(1, 2, 3);
     auto ans2 = fold_add(1, 2, 3);
     assert(ans1 == ans2);
+}
+
+void test_print() {
+    printk("my age is:{} my height is:{}\n", 18, 180);
+
+    printk("my name is:{} my weight is:{}. I say: hello world\n", "jack", 70);
+}
+
+int main() {
+
+    test_add();
+
+    test_print();
 }
--- doc/template/variadic-template.md
+++ doc/template/variadic-template.md
@@ -1,2 +1,3 @@
 ## 题目描述
-利用variadic-template特性实现求和函数
+1. 利用variadic-template特性实现求和函数
+2. 利用variadic-template特性实现类似python的format打印
